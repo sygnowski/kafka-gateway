5ed403d65ada9a6ca0b5685cd720dd230eee78bb
nefro85 <mariusz.sygnowski@gmail.com>
2022-03-19 13:20:00 +0100

    working the message publishing to kafka feature

--- internal/kafka/integration.go
+++ internal/kafka/integration.go
@@ -2,19 +2,24 @@ package kafka
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type Properties struct {
-	Server string
+	Server       string
+	PublishTopic string
 }
 
 type KafkaProducer struct {
-	prod *kafka.Producer
+	prod            *kafka.Producer
+	cfg             *Properties
+	producerChannel chan *kafka.Message
 }
 
-func (prod *KafkaProducer) Init(props *Properties) {
+func (this *KafkaProducer) Init(props *Properties) {
 	fmt.Println("making kafka producer")
 
 	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": props.Server})
@@ -23,25 +28,58 @@ func (prod *KafkaProducer) Init(props *Properties) {
 
 		panic(err)
 	}
-	prod.prod = p
+	this.prod = p
+	this.cfg = props
 
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				m := ev
-				if m.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
-				} else {
-					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-				}
-				return
+	this.producerChannel = p.ProduceChannel()
 
-			default:
-				fmt.Printf("Ignored event: %s\n", ev)
-			}
+	go this.handleEvents(p.Events())
+
+}
+
+func (prod *KafkaProducer) Publish(w http.ResponseWriter, req *http.Request) {
+	var bodyBytes []byte
+	var err error
+
+	if req.Body != nil {
+		bodyBytes, err = io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Printf("Body reading error: %v", err)
+			return
 		}
-	}()
+		defer req.Body.Close()
+	}
+	prod.publishToKafka(bodyBytes)
 
+}
+
+func (prod *KafkaProducer) publishToKafka(data []byte) {
+	fmt.Println("Publishing to Kafka...")
+	fmt.Println(string(data))
+
+	prod.producerChannel <- &kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &prod.cfg.PublishTopic,
+			Partition: 0},
+		Value: data}
+}
+
+func (prod *KafkaProducer) handleEvents(events chan kafka.Event) {
+
+	for e := range events {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			m := ev
+			if m.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+			} else {
+				fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+					*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+			}
+			return
+
+		default:
+			fmt.Printf("Ignored event: %s\n", ev)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 
 	"s7i.io/kafka-gateway/internal/kafka"
@@ -12,11 +13,15 @@ import (
 func main() {
 
 	prod := kafka.KafkaProducer{}
-	prod.Init(&kafka.Properties{Server: "localhost:9092"})
+	prod.Init(&kafka.Properties{
+		Server:       os.Getenv("BROKER"),
+		PublishTopic: os.Getenv("PUBLISH_TOPIC"),
+	})
 
 	fmt.Println("running server")
 
 	http.HandleFunc("/hello", hndHello)
+	http.HandleFunc("/", prod.Publish)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
