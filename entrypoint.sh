8fdacc8b417c4b901099d695ac75ad4950d21f89
nefro85 <mariusz.sygnowski@gmail.com>
2022-03-20 12:43:28 +0100

    not working...

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@ echo Gateway Entrypoint
 echo
 echo BROKER: $BROKER
 echo PUBLISH_TOPIC: $PUBLISH_TOPIC
+echo SUBSCRIBE_TOPIC: $SUBSCRIBE_TOPIC
+echo SUBSCRIBE_GROUP_ID: $SUBSCRIBE_GROUP_ID
+echo TIMEOUT: $TIMEOUT
 echo
 
-./bin/gateway
+ls -la /app
+
+/app/gateway
