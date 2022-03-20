d12f1b1a34a9480ee24e5727080c6114909e32e1
nefro85 <mariusz.sygnowski@gmail.com>
2022-03-20 12:46:18 +0100

    entrypoint improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@ echo Gateway Entrypoint
 echo
 echo BROKER: $BROKER
 echo PUBLISH_TOPIC: $PUBLISH_TOPIC
+echo SUBSCRIBE_TOPIC: $SUBSCRIBE_TOPIC
+echo SUBSCRIBE_GROUP_ID: $SUBSCRIBE_GROUP_ID
+echo TIMEOUT: $TIMEOUT
 echo
 
 ./bin/gateway
